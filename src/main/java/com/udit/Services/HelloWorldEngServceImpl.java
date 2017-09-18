bfaed780bbf8d4e07c6e2aab41e40613abd081bb
udittyagi <udittyagi2@gmail.com>
2017-09-19 00:03:53 +0530

    Java Configuration and Profiles

--- src/main/java/com/udit/Services/HelloWorldEngServceImpl.java
+++ src/main/java/com/udit/Services/HelloWorldEngServceImpl.java
@@ -1,15 +1,8 @@
 package com.udit.Services;
-
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
 /**
  * Created by udit on 18/9/17.
  */
-@Component
-@Profile("default")
-//to mark it as default as well as english profile you can do @Profile({"default","english"})
+
 public class HelloWorldEngServceImpl implements HelloWorldService{
     @Override
     public String getGreeting() {
--- src/main/java/com/udit/Services/HelloWorldSpanImpl.java
+++ src/main/java/com/udit/Services/HelloWorldSpanImpl.java
@@ -1,13 +1,10 @@
 package com.udit.Services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 
 /**
  * Created by udit on 18/9/17.
  */
-@Component
-@Profile("spanish")
+
 public class HelloWorldSpanImpl implements HelloWorldService {
 
     @Override
